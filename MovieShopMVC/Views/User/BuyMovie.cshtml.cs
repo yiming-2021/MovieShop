2a1a566e7aa1b07a89f227c30b75c8f236ef4fbb
Yiming <90655469+yiming-2021@users.noreply.github.com>
2021-11-12 18:48:31 -0500

    Add files via upload

---
+++ MovieShopMVC/Views/User/BuyMovie.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MovieShopMVC.Views.User
+{
+    public class BuyMovie : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}

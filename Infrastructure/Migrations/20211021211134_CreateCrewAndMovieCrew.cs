812247d2fee90c8b7c68d633fefc9fb00ecca454
Yiming <90655469+yiming-2021@users.noreply.github.com>
2021-11-12 18:46:38 -0500

    Add files via upload

---
+++ Infrastructure/Migrations/20211021211134_CreateCrewAndMovieCrew.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class CreateCrewAndMovieCrew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Character",
+                table: "MovieCast",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Crew",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TmdbUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProfilePath = table.Column<string>(type: "nvarchar(2084)", maxLength: 2084, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Crew", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieCrew",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    CrewId = table.Column<int>(type: "int", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    Job = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieCrew", x => new { x.MovieId, x.CrewId });
+                    table.ForeignKey(
+                        name: "FK_MovieCrew_Crew_CrewId",
+                        column: x => x.CrewId,
+                        principalTable: "Crew",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieCrew_Movie_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieCrew_CrewId",
+                table: "MovieCrew",
+                column: "CrewId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieCrew");
+
+            migrationBuilder.DropTable(
+                name: "Crew");
+
+            migrationBuilder.DropColumn(
+                name: "Character",
+                table: "MovieCast");
+        }
+    }
+}
